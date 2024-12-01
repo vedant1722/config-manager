76ffff75810b18ca9bc1c7dfc1ce310d06fdb973
vedant <vedant@railofy.com>
2024-12-01 15:08:00 +0530

    ConfigService get and getMany implementation

---
+++ src/core/ConfigService.ts
@@ -0,0 +1,56 @@
+import { JsonKeyValue, Optional } from "../types/common.types";
+import LocalCache from "./LocalCache";
+import StoreContract from "./StoreContract";
+
+export default class ConfigService {
+    protected store: StoreContract;
+    protected localCache: LocalCache;
+
+    constructor({ store, localCache }: {
+        store: StoreContract,
+        localCache: LocalCache
+    }) {
+        this.store = store;
+        this.localCache = localCache;
+    }
+
+    private async updateCacheConfig(input: {
+        appId: string, env: string, version: string
+    }) {
+        const { appId, env, version } = input;
+        const config = await this.store.fetchConfig({ appId, env, version });
+        this.localCache.setLocalConfig({ appId, env, version }, config);
+    }
+
+    async findOrUpdateCache(input: {
+        appId: string, env: string, version: string, jsonQuery: string
+    }) {
+        const { appId, env, version, jsonQuery } = input;
+        let cached = this.localCache.get({ appId, env, version, jsonQuery });
+
+        if(!cached) {
+            await this.updateCacheConfig({ appId, env, version });
+        }
+
+        cached = this.localCache.get({ appId, env, version, jsonQuery });
+
+        return cached;
+    }
+
+    public async get(input: {
+        appId: string, env: string, version: string, key: string
+    }) {
+        const { appId, env, version, key } = input;
+        return await this.findOrUpdateCache({ appId, env, version, jsonQuery: key });
+    }
+
+    public async getMany({ appId, env, version, keys }: { appId: string, env: string, version: string,  keys: string[] }) {
+        const output: { [key: string]: JsonKeyValue } = {};
+        for(const key of keys) {
+            const value = await this.get({ appId, env, version, key });
+            value !== undefined && (output[key] = value);
+        }
+
+        return output;
+    }
+}
